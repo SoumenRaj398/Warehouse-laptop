563ea09d0cdde7fcf6cdf782174ad6f3ce48998d
SoumenRaj398 <soumenraj66@gmail.com>
2022-05-19 02:52:03 +0600

    myself

--- src/Pages/Myself/Myself.js
+++ src/Pages/Myself/Myself.js
@@ -5,7 +5,7 @@ const Myself = () => {
         <div className='container'>
              <h3 className='text-center text-danger'>About My Aim</h3>
              <h4>
-                 I am a graduate student of computer engineering.but i was very weak in programming .but now after this p-hero course i am confident about me a lot and i want to be a full stack web developer .its my one and only dream now
+                
              </h4>
             
         </div>
